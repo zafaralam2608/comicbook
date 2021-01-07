e64b2678634d5b310f67bbeca380963fc5e955a7
Zafar <zafaralam2608@gmail.com>
2021-01-07 21:26:05 +0530

    initial react code

---
+++ client/src/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import Application from "./main/Application";
+
+ReactDOM.render(
+    <React.StrictMode>
+        <Application/>
+    </React.StrictMode>,
+    document.getElementById("root")
+);
